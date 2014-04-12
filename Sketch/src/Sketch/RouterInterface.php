ad6a6544f3532efba5aa20d40b9dc4e83ba3b2c4
pnoonan <pnoonan@arcstone.com>
2014-04-12 14:09:24 -0500

    move tests file into top level director

--- Sketch/src/Sketch/RouterInterface.php
+++ Sketch/src/Sketch/RouterInterface.php
@@ -3,7 +3,6 @@
 namespace Sketch;
 
 interface RouterInterface {
-
     public function resolve();
     public function post(array $params, $controller);
     public function get(array $params, $controller);
